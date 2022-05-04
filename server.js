e864d8e0fa15deaac4ae2365308b80249d3379cc
3120ZX <3120eduardo@gmail.com>
2022-05-04 11:53:37 +0700

    4/5/2022

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const app = express()
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+console.log('Starting server')
+
 app.get('/', (req,res)=>{
     res.render('hello.html')
-    console.log('kepin')
-    
+    console.log('req received')
 })
 
 app.listen(8000)
